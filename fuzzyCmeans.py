42e5311429a146bdcc7c28a2b1aaba3292ee3df6
shehabeldeenibrahim <shehab_eldiin@hotmail.com>
2020-05-29 00:05:18 +0200

    bug fixes in FCM and added prints

--- fuzzyCmeans.py
+++ fuzzyCmeans.py
@@ -4,6 +4,17 @@ import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
 from sklearn.metrics import pairwise_distances_argmin
+import math
+
+def SSE(data, labels, centroids, k):
+
+    # Calculate mean squared error
+    summation = 0
+    for i in range(k):
+        diff = (data[i] - centroids[labels[i]])**2
+        summation += diff
+    return math.sqrt((summation[0]**2 + summation[1]**2))
+
 
 def FCM(data, k, r, m):
     # Run algorithm r times and pick the least MSE
@@ -14,57 +25,76 @@ def FCM(data, k, r, m):
     weights = np.zeros((data.shape[0], k))
 
     # Initialize with random centroids
-    # centroids = (data.sample(k))
-    # centroids = centroids.values
     centroids = np.random.rand(k, data.shape[1])
     df = data
     data = data.values
+    
+    for a in range(r):
+        while(1):
 
-    while(1):
-
-        # Compute new membership weights
-        for i in range(weights.shape[0]):
-            for j in range(weights.shape[1]):
-                summation = 0
-                for a in range(k):
-                    # If point is the centroid
-                    if np.all(data[i] == centroids[j]) or np.all(data[i] == centroids[a]):
-                        summation += 0.0001
-                    else:
+            # Compute new membership weights
+            for i in range(weights.shape[0]):
+                for j in range(weights.shape[1]):
+                    summation = 0
+                    for a in range(k):
                         summation+= (np.linalg.norm(data[i]-centroids[j]) / np.linalg.norm(data[i]-centroids[a])) ** (2/ (m - 1))
-                weights[i][j] = 1/summation
+                    weights[i][j] = 1/summation
 
-        # Compute new centroids
-        for i in range(k):
-            summation1 = np.array([0.0, 0.0])
-            for x in range(data.shape[0]):
-                summation1 += ((weights[x][i] ** m) * data[x])
-            newCentroids[i] = summation1 / weights[:,i].sum()
+            # Compute new centroids
+            for i in range(k):
+                summation1 = 0
+                for x in range(data.shape[0]):
+                    summation1 += ((weights[x][i] ** m) * data[x])
+                newCentroids[i] = summation1 / weights[:,i].sum()
 
-        # Check for convergence
-        if np.all(centroids == newCentroids):
-            break
-        centroids = newCentroids
+            # Check for convergence
+            if np.all(centroids == newCentroids):
+                break
+            centroids = newCentroids
 
-    # Put labels
-    labels = []
-    for i in range(data.shape[0]):
-        weightedDistaces = []
-        for j in range(k):
-            weightedDistaces.append(weights[i][j]*np.linalg.norm(data[i]-centroids[j]))
-        labels.append(np.argmin(weightedDistaces))
-    print("")
+        # Put labels
+        labels = []
+        for i in range(data.shape[0]):
+            weightedDistaces = []
+            for j in range(k):
+                weightedDistaces.append(np.linalg.norm(data[i]-centroids[j]))
+            labels.append(np.argmin(weightedDistaces))
+
+        # Calculate mean squared errors
+        # Push data of the rth run into the arrays
+        mseArray.append(SSE(data, labels, centroids, k))
+        labelsArray.append(labels)
+        centroidsArray.append(centroids)
+
+    # Get index of the minimum MSE
+    minIndex = mseArray.index(min(mseArray))
+
+    # Use labels and centroids of minimum MSE
+    mse = "SSE: " + str(mseArray[minIndex])    
+    centroids = centroidsArray[minIndex]
+    labels = labelsArray[minIndex]
+
+    print("SSE: " + str(mse))
+
     # Plot data clustered and centroids in red
-    #plt.title(mse)
+    plt.title(mse)
     plt.scatter(df[0],df[1],c=labels)
-    plt.scatter(centroids[:, 0],centroids[:, 1],c='red')
+    plt.scatter(centroids[:, 0],centroids[:, 1],c='red', marker='x')
     plt.xlabel('Col0')
     plt.ylabel('Col1')
-    plt.show()
+    plt.savefig('FCM_Results/'+ str(k) + '.png')
+    #plt.show()
         
 
 # Import dataset
 data = pd.read_csv('cluster_dataset.txt', sep="  ", header=None)
 
 # Run kmeans and plot
-FCM(data, k=5, r=5, m=2)
+i = 2
+
+print("###FCM Clustering started###")
+while(i < 6):
+    print("k = " + str(i))
+    FCM(data, k=i, r=5, m=1.001)
+    i+=1
+
--- kmeans.py
+++ kmeans.py
@@ -4,15 +4,16 @@ import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
 from sklearn.metrics import pairwise_distances_argmin
+import math
 
-def MSE(data, labels, centroids, k):
+def SSE(data, labels, centroids, k):
 
     # Calculate mean squared error
     summation = 0
     for i in range(k):
         diff = (data[i] - centroids[labels[i]])**2
         summation += diff
-    return (summation[0] + summation[1])**2
+    return math.sqrt((summation[0]**2 + summation[1]**2))
 
 def k_means(data, k, r):
     # Run algorithm r times and pick the least MSE
@@ -39,7 +40,7 @@ def k_means(data, k, r):
         
         # Calculate mean squared errors
         # Push data of the rth run into the arrays
-        mseArray.append(MSE(data.values, labels, centroids, k))
+        mseArray.append(SSE(data.values, labels, centroids, k))
         labelsArray.append(labels)
         centroidsArray.append(centroids)
 
@@ -47,7 +48,7 @@ def k_means(data, k, r):
     minIndex = mseArray.index(min(mseArray))
 
     # Use labels and centroids of minimum MSE
-    mse = "MSE: " + str(mseArray[minIndex])    
+    mse = "SSE: " + str(mseArray[minIndex])    
     centroids = centroidsArray[minIndex]
     labels = labelsArray[minIndex]
 
@@ -57,10 +58,14 @@ def k_means(data, k, r):
     plt.scatter(centroids[:, 0],centroids[:, 1],c='red')
     plt.xlabel('Col0')
     plt.ylabel('Col1')
-    plt.show()
+    plt.savefig('KM_Results/'+ str(k) + '.png')
+    #plt.show()
 
 # Import dataset
 data = pd.read_csv('cluster_dataset.txt', sep="  ", header=None)
 
 # Run kmeans and plot
-k_means(data, k=5, r=5)
+i = 2
+while(i < 6):
+    k_means(data, k=i, r=5)
+    i+=1
